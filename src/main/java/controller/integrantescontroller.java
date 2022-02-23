7e66671ec2a3d0594d4b7b003c1db93698352e00
perkles <maikosilvae@gmail.com>
2022-02-23 18:42:09 -0300

    2

--- src/main/java/controller/integrantescontroller.java
+++ src/main/java/controller/integrantescontroller.java
@@ -1,14 +1,27 @@
 package controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.duxusdesafio.model.Integrante;
+import repository.Integrantesrepository;
 
 @RestController
 public class integrantescontroller {
 	
+	@Autowired
+	private Integrantesrepository integrantesrepository;
+	
 	@GetMapping(path = "api/intregantes-id/{}")
-	public void buscarintegrantesporId() {
-		
+	public ResponseEntity<Optional<Integrante>> buscarintegrantesporId(
+			@PathVariable(name = "id", required = true)long idintegrantes) {
+		return ResponseEntity
+				.ok(integrantesrepository.findById(idintegrantes));
 	}
 
 }
--- src/main/java/controller/timescontroller.java
+++ src/main/java/controller/timescontroller.java
@@ -1,14 +1,26 @@
 package controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.duxusdesafio.model.Time;
+import repository.timesrepository;
 
 @RestController
 public class timescontroller {
 	
+	@Autowired
+	private timesrepository timesrepository;
+	
 	@GetMapping(path = "api/times-id/{}")
-	public void buscartimesporID() {
-		
+	public ResponseEntity<Optional<Time>> buscartimesporID(
+			@PathVariable(name = "id", required = true)long idtimes) {
+		return ResponseEntity.ok( timesrepository.findById(idtimes));
 	}
 
 }
