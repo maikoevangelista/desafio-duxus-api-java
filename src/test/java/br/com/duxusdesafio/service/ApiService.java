bfddeef983d61b3077993bfce9d35c80529cac33
perkles <maikosilvae@gmail.com>
2022-02-25 11:01:47 -0300

    4

---
+++ src/test/java/br/com/duxusdesafio/service/ApiService.java
@@ -0,0 +1,47 @@
+package br.com.duxusdesafio.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import br.com.duxusdesafio.model.Integrante;
+import br.com.duxusdesafio.model.Time;
+
+public class ApiService {
+
+	public Integrante integranteMaisUsado(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String funcaoMaisComum(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String franquiaMaisFamosa(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map<String, Long> contagemPorFranquia(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map<String, Long> contagemPorFuncao(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Time timeDaData(LocalDate data, List<Time> todosOsTimes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> timeMaisComum(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
